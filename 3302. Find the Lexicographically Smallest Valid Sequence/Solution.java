f4b423b293dd9377a047114cdce01045d66c1ae3
Bajwa-Ajai <ajayrko2001@gmail.com>
2026-08-11 20:35:17 +0200

    sol

---
+++ 3302. Find the Lexicographically Smallest Valid Sequence/Solution.java
@@ -0,0 +1,104 @@
+
+
+
+// You are given two strings word1 and word2.
+
+// A string x is called almost equal to y if you can change at most one character in x to make it identical to y.
+
+// A sequence of indices seq is called valid if:
+
+// The indices are sorted in ascending order.
+// Concatenating the characters at these indices in word1 in the same order results in a string that is almost equal to word2.
+// Return an array of size word2.length representing the lexicographically smallest valid sequence of indices. If no such sequence of indices exists, return an empty array.
+
+// Note that the answer must represent the lexicographically smallest array, not the corresponding string formed by those indices.
+
+ 
+
+// Example 1:
+
+// Input: word1 = "vbcca", word2 = "abc"
+
+// Output: [0,1,2]
+
+// Explanation:
+
+// The lexicographically smallest valid sequence of indices is [0, 1, 2]:
+
+// Change word1[0] to 'a'.
+// word1[1] is already 'b'.
+// word1[2] is already 'c'.
+// Example 2:
+
+// Input: word1 = "bacdc", word2 = "abc"
+
+// Output: [1,2,4]
+
+// Explanation:
+
+// The lexicographically smallest valid sequence of indices is [1, 2, 4]:
+
+// word1[1] is already 'a'.
+// Change word1[2] to 'b'.
+// word1[4] is already 'c'.
+// Example 3:
+
+// Input: word1 = "aaaaaa", word2 = "aaabc"
+
+// Output: []
+
+// Explanation:
+
+// There is no valid sequence of indices.
+
+// Example 4:
+
+// Input: word1 = "abc", word2 = "ab"
+
+// Output: [0,1]
+
+ 
+
+// Constraints:
+
+// 1 <= word2.length < word1.length <= 3 * 105
+// word1 and word2 consist only of lowercase English letters.
+
+public class Solution {
+    public int[] validSequence(String word1, String word2) {
+        int n=word1.length();
+        int m=word2.length();
+        int i=n-1,j=m-1;
+
+        int[] rightStringCommonCharacters= new int[n];
+        java.util.Arrays.fill(rightStringCommonCharacters,0);
+        int rightMatched=0;
+        while(i>=0){
+            if(j>=0 && word1.charAt(i)==word2.charAt(j)){
+                j--;
+                rightMatched++;
+            }
+            rightStringCommonCharacters[i]=rightMatched;
+            i--;
+        }
+        int[] ans=new int[m];
+        i=0;
+        j=0;
+        boolean canChange=true;
+        while(i<n &&j<m){
+            if(word1.charAt(i)==word2.charAt(j)){
+                ans[j]=i;
+                j++;
+            }else if(canChange==true && i+1<n && rightStringCommonCharacters[i+1]>=m-j-1){
+                canChange=false;
+                ans[j]=i;
+                j++;
+            }
+            i++;
+        }
+        if(j==m){
+            return ans;
+        }
+        return new int[0];
+    }
+}
